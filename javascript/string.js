db3f60fca7b3e633aab9ef1b74daf45bc2bc64a4
Pinkygs <pinkygs364@gmail.com>
2024-08-27 11:14:04 +0530

    commit all

---
+++ javascript/string.js
@@ -0,0 +1,5 @@
+var fName = "Raj"
+var lName ="Kapoor"
+var fullName = fName + " " + lName
+console.log("full Name is " + fullName)
+console.log(`full Name is ${fullName}`);
---
+++ javascript/variable.js
@@ -0,0 +1,6 @@
+var sName = "Raj"
+var age = 19
+var isPassed = true
+console.log("the student name is " + sName)
+sName ="Varun"
+console.log("The student name is " + sName)
